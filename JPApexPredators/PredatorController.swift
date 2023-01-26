748ec062f7e39bda713070839981e358b2ccbe93
Kenneth Jones <kennethjones1991@outlook.com>
2023-01-26 15:02:08 -0700

    End of lesson 3; create PredatorController class

---
+++ JPApexPredators/PredatorController.swift
@@ -0,0 +1,29 @@
+//
+//  PredatorController.swift
+//  JPApexPredators
+//
+//  Created by Kenneth Jones on 1/26/23.
+//
+
+import Foundation
+
+class PredatorController {
+    var apexPredators: [ApexPredator] = []
+    
+    init() {
+        decodeApexPredatorData()
+    }
+    
+    func decodeApexPredatorData() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+            } catch {
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+}
