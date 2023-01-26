cb38e591a8c56e1c90457365c1dc3aac031100b9
Kenneth Jones <kennethjones1991@outlook.com>
2023-01-26 14:21:51 -0700

    Initial project commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Kenneth Jones on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
