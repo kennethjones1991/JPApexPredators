30ec89a3772d488c58432f632f9d3b3cd41eaf00
Kenneth Jones <kennethjones1991@outlook.com>
2023-01-27 01:18:51 -0700

    End of lesson 11; menus, pickers, and animations

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -10,8 +10,11 @@ import SwiftUI
 struct ContentView: View {
     let apController = PredatorController()
     @State var sortAlphabetical = false
+    @State var currentFilter = "All"
     
     var body: some View {
+        apController.filterBy(type: currentFilter)
+        
         if sortAlphabetical {
             apController.sortByAlphabetical()
         } else {
@@ -30,13 +33,33 @@ struct ContentView: View {
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button {
-                        sortAlphabetical.toggle()
+                        withAnimation {
+                            sortAlphabetical.toggle()
+                        }
                     } label: {
                         if sortAlphabetical {
                             Image(systemName: "film")
                         } else {
                             Image(systemName: "textformat")
                         }
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu {
+                        Picker("Filter", selection: $currentFilter.animation()) {
+                            ForEach(apController.typeFilters, id: \.self) { type in
+                                HStack {
+                                    Text(type)
+                                    
+                                    Spacer()
+                                    
+                                    Image(systemName: apController.typeIcon(for: type))
+                                }
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
                     }
                 }
             }
